b1491167afb81c1ffd9566cc5ecfdb97382b45c3
Julio Muñoz Gallarin <pretonik1978@yahoo.es>
2016-11-16 20:22:45 +0100

    spring integration example

--- sse/src/main/java/com/example/SpeakerPublisher.java
+++ sse/src/main/java/com/example/SpeakerPublisher.java
@@ -3,28 +3,28 @@ package com.example;
 import java.util.Random;
 
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.stereotype.Service;
 
 import lombok.Data;
 
-@Service
+@Service("speakerPublisher")
 @Data
 public class SpeakerPublisher {
 
 	private static final int CLIENT_ID = 1;
 	private final ApplicationEventPublisher eventPublisher;
 
-
-	@Scheduled(fixedDelay=5000)
-	public void speak() {
+	@ServiceActivator(inputChannel="outputChannel")
+	public void speak(String s) {
 		
-		eventPublisher.publishEvent(new SpeechEvent(this, randomWord(6), CLIENT_ID));
+		eventPublisher.publishEvent(new SpeechEvent(this, s, CLIENT_ID));
 	}
 	
-	public  String randomWord(int length) {
+	public  String randomWord() {
 	    Random random = new Random();
-	    StringBuilder word = new StringBuilder(length);
+	    int length = 6;;
+		StringBuilder word = new StringBuilder( );
 	    for (int i = 0; i < length; i++) {
 	        word.append((char)('a' + random.nextInt(26)));
 	    }
--- sse/src/main/java/com/example/SseApplication.java
+++ sse/src/main/java/com/example/SseApplication.java
@@ -4,16 +4,52 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.core.Pollers;
+import org.springframework.integration.endpoint.MethodInvokingMessageSource;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
-@EnableScheduling
+@IntegrationComponentScan
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SseApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SseApplication.class, args);
 	}
+	
+	@Bean
+	public MessageSource<?> randomStringMessageSource(SpeakerPublisher publisher){
+		MethodInvokingMessageSource source = new MethodInvokingMessageSource();
+		source.setObject(publisher);
+		source.setMethodName("randomWord");
+		return source;
+		
+	}
+	
+	@Bean
+	DirectChannel inputChannel(){
+		return new DirectChannel();
+	}
+	
+	@Bean 
+	DirectChannel outputChannel(){
+		return new DirectChannel();
+	}
+	
+	@Bean 
+	IntegrationFlow myFlow(SpeakerPublisher publisher){
+		return IntegrationFlows.from(this.randomStringMessageSource(publisher),c -> c.poller(Pollers.fixedDelay(5000)))
+				.channel(inputChannel())
+				.transform(p -> p.toString().toUpperCase())
+				.channel(outputChannel())
+				.get();
+	}
+	
 }
